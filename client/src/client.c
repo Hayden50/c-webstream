80cf94608a1be80958a4605ae6d7e2c6c60571ac
Hayden50 <haydenrieder50@hotmail.com>
2022-12-27 18:06:19 -0500

    Connection Created

--- client/src/client.c
+++ client/src/client.c
@@ -1,7 +1,34 @@
+#include <netinet/in.h>
+#include <string.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <sys/_endian.h>
 #include <sys/socket.h>
+#include <arpa/inet.h>
 
 int main() {
-  int client_socket = socket(AF_LOCAL, SOCK_STREAM, 0);
+  int client_socket = socket(AF_INET, SOCK_STREAM, 0);
+
+  struct sockaddr_in server_address;
+  server_address.sin_family = AF_INET;
+  server_address.sin_port = htons(8001);
+  
+  // Sets IP of server to connect to and converts to binary form
+  if (inet_pton(AF_INET, "127.0.0.1", &server_address.sin_addr) < 0) {
+    printf("Binary conversion failed\n");
+    return 0;
+  }
+
+  // Connects to server using client socket and server socket data
+  if (connect(client_socket, (struct sockaddr *)&server_address, sizeof(server_address)) < 0) {
+    printf("Connection Failed\n");
+    return 0;
+  }
+
+  printf("Connection Successful\n");
+
+  char *buffer = "Hello World";
+  write(client_socket, (void *)buffer, strlen(buffer));
+
   return 0;
 }
--- server/src/server.c
+++ server/src/server.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
+#include <unistd.h>
 #include "../server-headers/socket.h"
 #define PORT 8001
 
 int main() {
-  printf("Server running on %d\n", PORT);
+  setbuf(stdout, NULL);
   
+  // Socket bound and listening at localhost:PORT
   http_socket http_s;
   create_socket(&http_s, PORT);
+
+  int new_socket = accept(http_s.socket, NULL, NULL);
+
+  char buffer[1024];
+  read(new_socket, buffer, 1024);
+  printf("%s\n", buffer);
 
   return 0;
 }
